f84a54ef278eaa45e35f4585cb38b80dff8fe8e8
PrestonHunter <prestonhunter2018@gmail.com>
2023-02-03 11:11:37 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", modify);
+
+function modify(){
+    text = document.querySelector("#text");
+    text.textContent = "This is really cool!";
+}
